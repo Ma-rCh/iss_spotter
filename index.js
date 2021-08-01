73fe065830ed9a5c8a3066348e89d3ae5431cf68
Your Name <you@example.com>
2021-08-01 18:07:19 +0000

    ip1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// index.js
+const { fetchMyIP } = require('./iss');
 
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return JSON.parse(body).ip;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
--- iss.js
+++ iss.js
@@ -1,1 +1,27 @@
+var http = require('http');
+var fs = require('fs');
+const request = require('request');
+const url = 'https://api.ipify.org?format=json';
 
+
+
+const fetchMyIP = function(callback) { 
+  // use request to fetch IP address from JSON API
+  request(url, (error, response, body) => {
+ 
+    if (error) {
+      return callback(error, null);
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+  
+   callback(null,JSON.parse(body).ip);
+ });
+}
+
+
+module.exports = { fetchMyIP };
